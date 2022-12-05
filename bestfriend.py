e9a8481fbceddf5beb16626266ffd92e1b85f508
Kara Geerdink <geerdink.kara@gmail.com>
2022-12-05 10:05:40 -0500

    My weird take on this

--- bestfriend.py
+++ bestfriend.py
@@ -1,3 +1,14 @@
 first_name = input("What is your name? ")
 print(f"Hello {first_name}!")
 
+color = input("What is your favorite color? ")
+print(f"I also like the color {color}.")
+
+animal = input("What is your favorite animal? ")
+print(f"I also like {animal}, but personally, I'm partial to pythons.")
+
+food = input("How about your favorite food? ")
+print(f"{food} sounds really good. Sadly, I don't have taste buds. Woe is me.")
+
+
+
